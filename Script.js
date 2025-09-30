ba6060f3a5ac839c067ce5fd0403996bc8ee2176
Nicojerson <nicojersonsky1234@gmail.com>
2025-09-30 19:31:44 -0300

    b

--- Script.js
+++ Script.js
@@ -1,5 +1,11 @@
 import fs from "fs";
 
 let listaordenada = [71, 1, 2, 116, 3, 84, 4, 5, 81, 74, 6, 7, 8, 93, 68, 9, 104, 10, 65, 11, 136];
-import fs from "fs";
+let lectura = []
 
+for (let i = 0; i<listaordenada.length; i ++){
+    
+    let lecturadei = fs.readFileSync ("parte" + listaordenada + ".txt", "utf-8")
+    lectura.push (lecturadei)
+}
+fs.writeFileSync ("TextoCompleto.txt", JSON.stringify(lectura))
